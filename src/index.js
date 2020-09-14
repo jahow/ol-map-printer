959120c47aa69a49b3cca45da8c7506cc6fb4740
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-09-14 10:07:52 +0200

    better worker handling for firefox

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ window.addEventListener('load', () => {
 
     printBtn.disabled = true
 
+    if (navigator.serviceWorker.controller) {
+        console.log(`This page is currently controlled by: ${navigator.serviceWorker.controller}`);
+    } else {
+        console.log('This page is not currently controlled by a service worker.');
+    }
+
     printBtn.addEventListener('click', () => {
         navigator.serviceWorker.controller.postMessage({
             type: "request",
@@ -13,12 +19,8 @@ window.addEventListener('load', () => {
 
     navigator.serviceWorker.onmessage = function (event) {
         const msg = event.data
-        console.log(msg)
+        console.log('Message received by main thread', msg)
         switch (msg.type) {
-            case 'ready':
-                console.log('Worker is ready')
-                printBtn.disabled = false
-                break;
             case 'notsupported':
                 console.log('OffscreenCanvas not supported :(')
                 break;
@@ -27,15 +29,27 @@ window.addEventListener('load', () => {
 
     navigator.serviceWorker.register('worker.js').then(
         registration => {
+
+            if(registration.installing) {
+                console.log('Service worker installing');
+            } else if(registration.waiting) {
+                console.log('Service worker installed');
+            } else if(registration.active) {
+                console.log('Service worker active');
+            }
+
             console.log('Service worker registration:', registration)
-            printBtn.disabled = false
             registration.addEventListener('updatefound', () => {
                 console.log('A service worker update was found')
-                printBtn.disabled = true
             })
         },
         error => {
             console.log('Service worker registration failed:', error)
         }
     )
+
+    navigator.serviceWorker.ready.then(() => {
+        console.log('Service worker ready')
+        printBtn.disabled = false
+    })
 })
